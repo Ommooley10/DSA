8d003c5e7cd9ef3283fc6d503ad844de2658da74
Ommooley10 <omvikas1001@gmail.com>
2025-03-16 12:00:43 +0530

    updated a file

--- Math/noOfValid3DigitNo.cpp
+++ Math/noOfValid3DigitNo.cpp
@@ -6,7 +6,6 @@ You are given an array of digits called digits.
 Your task is to determine the number of distinct three-digit even numbers that can be formed using these digits.
 Note: Each copy of a digit can only be used once per number, and there may not be leading zeros.
 
-
 Example 1:
 Input: digits = [1,2,3,4]
 Output: 12
