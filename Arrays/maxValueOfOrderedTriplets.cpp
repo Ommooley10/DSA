dbdc04b65bc3e07842c3ec839fd14c520d7ed5fe
Ommooley10 <omvikas1001@gmail.com>
2025-04-02 23:33:36 +0530

    added file

---
+++ Arrays/maxValueOfOrderedTriplets.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*You are given a 0-indexed integer array nums.
+Return the maximum value over all triplets of indices (i, j, k) such that i < j < k. 
+If all such triplets have a negative value, return 0.
+The value of a triplet of indices (i, j, k) is equal to (nums[i] - nums[j]) * nums[k].
+
+Example 1:
+Input: nums = [12,6,1,2,7]
+Output: 77
+Explanation: The value of the triplet (0, 2, 4) is (nums[0] - nums[2]) * nums[4] = 77.
+It can be shown that there are no ordered triplets of indices with a value greater than 77. 
+
+Example 2:
+Input: nums = [1,10,3,4,19]
+Output: 133
+Explanation: The value of the triplet (1, 2, 4) is (nums[1] - nums[2]) * nums[4] = 133.
+It can be shown that there are no ordered triplets of indices with a value greater than 133.
+
+Example 3:
+Input: nums = [1,2,3]
+Output: 0
+Explanation: The only ordered triplet of indices (0, 1, 2) has a negative value of (nums[0] - nums[1]) * nums[2] = -3. Hence, the answer would be 0.*/
+
+class Solution {
+    public:
+        long long maximumTripletValue(vector<int>& nums) {
+            int n = nums.size();
+            long long maxAns = 0;
+    
+            // We will keep track of the maximum value of nums[i] - nums[j] for each j
+            long long maxDiff = LLONG_MIN;
+    
+            for (int j = 1; j < n - 1; j++) {
+                // Update maxDiff for the current j
+                for (int i = 0; i < j; i++) {
+                    maxDiff = max(maxDiff, (long long)nums[i] - nums[j]);
+                }
+    
+                // Now calculate the value for each k > j
+                for (int k = j + 1; k < n; k++) {
+                    long long ans = maxDiff * nums[k];
+                    maxAns = max(maxAns, ans);
+                }
+            }
+    
+            return maxAns;
+        }
+    };
