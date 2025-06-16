ca2485dadfb9074de3c4e4e4598c83b0d00913c3
Ommooley10 <omvikas1001@gmail.com>
2025-06-16 17:28:34 +0530

    max width of binary tree

---
+++ Binary Trees/maxWidthOfBinaryTree.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+// LEETCODE: 662
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int widthOfBinaryTree(TreeNode* root) {
+        deque<pair<TreeNode*, long long>> dq;
+        long long maxWidth  = 0;
+        if(root == NULL) return maxWidth;
+        dq.push_back({root,0});
+
+        while(!dq.empty()){
+            long long n = dq.size();
+            long long L = dq.front().second;
+            long long R = dq.back().second;
+
+            maxWidth = max(maxWidth,R-L+1);
+
+            while(n--){
+                TreeNode* curr = dq.front().first;
+                long long idx = dq.front().second;
+                dq.pop_front();
+
+                if(curr->left != NULL) dq.push_back({curr->left, 2*idx+1});
+                if(curr->right != NULL) dq.push_back({curr->right, 2*idx+2});
+            }
+        }
+        return maxWidth;
+    }
+};
