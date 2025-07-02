2336d2061520baa4bd56892edb709c4ae28dafc8
Ommooley10 <omvikas1001@gmail.com>
2025-07-02 14:03:13 +0530

    Binary tree from inorder and postorder

--- Binary Trees/BTfromInorderAndPreorder.cpp
+++ Binary Trees/BTfromInorderAndPreorder.cpp
@@ -32,17 +32,7 @@ Example 2:
 Input: preorder = [-1], inorder = [-1]
 Output: [-1]*/
 
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+
 class Solution {
 public:
     TreeNode* solve(vector<int>& preorder, vector<int>& inorder, int start, int end, int& idx){
