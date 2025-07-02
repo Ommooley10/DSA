e7dcea4fbd5a0be219eca901a2ea0d955db5a36e
Ommooley10 <omvikas1001@gmail.com>
2025-07-02 23:33:06 +0530

    code fixes

--- Binary Trees/Binary Search Trees/searchInBST.cpp
+++ Binary Trees/Binary Search Trees/searchInBST.cpp
@@ -24,17 +24,6 @@ Example 2:
 Input: root = [4,2,7,1,3], val = 5
 Output: []*/
 
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
     TreeNode* searchBST(TreeNode* root, int val) {
