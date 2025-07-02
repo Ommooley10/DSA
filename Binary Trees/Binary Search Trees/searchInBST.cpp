7fee8959c011303f4a06f84c562ca367e24eb04d
Ommooley10 <omvikas1001@gmail.com>
2025-07-02 23:46:27 +0530

    code changes

--- Binary Trees/Binary Search Trees/searchInBST.cpp
+++ Binary Trees/Binary Search Trees/searchInBST.cpp
@@ -30,7 +30,7 @@ public:
         if(root == NULL) return NULL;
         if(root->val == val) return root;
 
-        if(root->val > val){
+        if(root->val > val){    
             return searchBST(root->left,val);
         }else{
             return searchBST(root->right, val);
